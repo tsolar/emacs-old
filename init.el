cde4e331873ac46cef696b4ca24421861462bd4e
Tomás Solar Castro <tsolar@gmail.com>
2014-05-13 18:14:40 -0400

    removed elscreen config

--- init.el
+++ init.el
@@ -607,31 +607,6 @@
 (require 'vcl-mode)
 
 
-;; ;;;elscreen
-
-;; added from git
-(add-to-list 'load-path "~/.emacs.d/elscreen")
-(load "elscreen" "ElScreen" t)
-(elscreen-start)
-(setq elscreen-prefix-key "\C-z")
-
-;; (defun elscreen-frame-title-update ()
-;;   (when (elscreen-screen-modified-p 'elscreen-frame-title-update)
-;;     (let* ((screen-list (sort (elscreen-get-screen-list) '<))
-;;  	   (screen-to-name-alist (elscreen-get-screen-to-name-alist))
-;;  	   (title (mapconcat
-;;  		   (lambda (screen)
-;;  		     (format "%d%s %s"
-;;  			     screen (elscreen-status-label screen)
-;;  			     (get-alist screen screen-to-name-alist)))
-;;  		   screen-list " ")))
-;;       (if (fboundp 'set-frame-name)
-;;  	  (set-frame-name title)
-;;  	(setq frame-title-format title)))))
-
-;; (eval-after-load "elscreen"
-;;   '(add-hook 'elscreen-screen-update-hook 'elscreen-frame-title-update))
-
 
 ;; Load ERC
 ;(add-to-list 'load-path "~/Descargas/erc/")
